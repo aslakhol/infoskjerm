6b5059fe5a8daa81bf0199d0ffa9bb147cf217ed
Aslak Hollund <aslak.hollund@nav.no>
2020-06-29 13:43:33 +0200

    Updates

---
+++ precache-manifest.08d522b4d836b6cc7e2740eecb9a4fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308c4a79b2b291db40d4baad7d1dec29",
+    "url": "/inforskjem/index.html"
+  },
+  {
+    "revision": "24680783b8003b886874",
+    "url": "/inforskjem/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25f0cc813811c1fe3965",
+    "url": "/inforskjem/static/js/2.c7db703d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inforskjem/static/js/2.c7db703d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24680783b8003b886874",
+    "url": "/inforskjem/static/js/main.afc87109.chunk.js"
+  },
+  {
+    "revision": "033daca2f7d5b7d510e5",
+    "url": "/inforskjem/static/js/runtime-main.08099203.js"
+  }
+]);
